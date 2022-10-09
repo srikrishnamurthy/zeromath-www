ca0a47241f261b960bda37326dddbd0e050e4b79
Srinidhi Krishnamurthy <srinidhi0912@gmail.com>
2022-10-09 02:43:53 -0500

    vercel is my compiler

--- zeromath/pages/post/[slug].tsx
+++ zeromath/pages/post/[slug].tsx
@@ -2,7 +2,7 @@ import Header from "../../components/Header"
 import {sanityClient, urlFor} from "../../sanity";
 
 interface Props {
-    post: Post;
+    post: typeof Post;
 }
 
 function Post({post}: Props){
